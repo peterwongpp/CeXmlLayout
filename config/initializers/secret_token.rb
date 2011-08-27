16e667b0147664bce78bde42c00b7d931cb87b06
PeterWong <peter@peterwongpp.com>
2011-08-27 23:56:11 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CeXmlLayout::Application.config.secret_token = '4775a53a16852c15c96a19ee33f70ab2221d99bb6a07f8d702cd7f357ab1cadd6bd4bf216c7d3a35747c88517ff68cd83ef28ac1538ad8dc2e38e56a7f79707c'
