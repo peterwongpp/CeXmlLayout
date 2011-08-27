fe1c3504ad131577519e871ece129321f67254fb
PeterWong <peter@peterwongpp.com>
2011-08-28 00:41:25 +0800

    added scaffold for song

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 CeXmlLayout::Application.routes.draw do
+  resources :songs
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+    root :to => 'songs#index'
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
