2a981c60789f80380975a0c62b60e1084988badc
PeterWong <peter@peterwongpp.com>
2011-08-28 01:02:34 +0800

    made the songs#index to use the xml builder

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,7 +6,7 @@ class SongsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @songs }
+      format.xml
     end
   end
 
---
+++ app/views/songs/index.xml.builder
@@ -0,0 +1,9 @@
+xml.instruct!
+
+xml.songs do
+  @songs.each do |song|
+    xml.song do
+      xml.name song.name
+    end
+  end
+end
